4b0be71ebaa59e1911eeed302878673c0225a8c0
steven <steven.lee.bus@gmail.com>
2022-04-13 08:54:41 -0400

    feat: integreate metamask for web3 api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { getUserInfo } from "../web3/users";
 
 export default class IndexPage extends React.Component {
+	logUser = async () => {
+		const userInfo = await getUserInfo(1);
+		console.log(userInfo);
+	};
+
 	render() {
-		return <h1>hello world!</h1>;
+		return (
+			<div>
+				<button onClick={this.logUser}>Get user with ID 1</button>
+			</div>
+		);
 	}
 }
