8ea42a6aa268d89443a7334cdd4f8c2c2f5fb22f
steven <steven.lee.bus@gmail.com>
2022-04-19 22:47:11 -0400

    feat: add header with user name

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,9 +3,32 @@ import Link from "next/link";
 
 import { Center } from "./Layout";
 import Logotype from "../icons/logotype.svg";
+import { getLoggedInUserId, getUserInfo } from "../web3/users";
+import Nav from "./Nav";
 
 export default class Header extends React.Component {
+	state = {
+		loggedIn: false,
+		userInfo: {},
+	};
+
+	async componentDidMount() {
+		const userId = await getLoggedInUserId();
+
+		try {
+			const userInfo = await getUserInfo(userId);
+
+			this.setState({
+				loggedIn: true,
+				userInfo,
+			});
+		} catch (err) {
+			console.error("Couldn't find logged in user", err);
+		}
+	}
+
 	render() {
+		const { loggedIn, userInfo } = this.state;
 		return (
 			<header>
 				<Center>
@@ -14,6 +37,8 @@ export default class Header extends React.Component {
 							<img {...Logotype} />
 						</a>
 					</Link>
+
+					<nav>{loggedIn && <Nav userInfo={userInfo} />}</nav>
 				</Center>
 
 				<style jsx>{`
--- client/web3/users.js
+++ client/web3/users.js
@@ -5,11 +5,36 @@ import UserController from "./artifacts/UserController.json";
 
 export const getUserInfo = async (userId) => {
 	const storage = await getInstance(UserStorage);
-	const { id, username } = await storage.profiles.call(userId);
+	const profile = await storage.profiles.call(userId);
+
+	const { id, username, firstName, lastName, bio, gravatarEmail } = profile;
+
+	if (!parseInt(id)) throw "Couldn't find user!";
+
 	return {
 		id: parseInt(id),
 		username: web3Obj.utils.toAscii(username).replace(/\u0000/g, ""),
+		firstName: web3Obj.utils.toAscii(firstName).replace(/\u0000/g, ""),
+		lastName: web3Obj.utils.toAscii(lastName).replace(/\u0000/g, ""),
+		bio,
+		gravatarEmail,
 	};
+};
+
+export const getLoggedInUserId = async () => {
+	try {
+		await ethereum.enable();
+		const addresses = await eth.getAccounts();
+
+		if (!addresses) return;
+
+		const storage = await getInstance(UserStorage);
+		const userId = await storage.addresses.call(addresses[0]);
+
+		return parseInt(userId);
+	} catch (err) {
+		console.error("Err:", err);
+	}
 };
 
 export const createUser = async (...params) => {
