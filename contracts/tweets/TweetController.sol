5fff32b93f1b23f823d416b29f07e47e0d269bcd
steven <steven.lee.bus@gmail.com>
2022-04-20 05:17:03 -0400

    update: getTweetIdsFromuser function

--- contracts/tweets/TweetController.sol
+++ contracts/tweets/TweetController.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.8.10;
 import '../helpers/BaseController.sol';
 import '../ContractManager.sol';
 import './TweetStorage.sol';
+import "../users/UserStorage.sol";
 
 contract TweetController is BaseController {
 
-  function createTweet(uint _userId, string memory _text) public returns(uint) {
+  function createTweet(string memory _text) public returns(uint) {
     ContractManager _manager = ContractManager(managerAddr);
+
+    address _userStorageAddr = _manager.getAddress("UserStorage");
+    UserStorage _userStorage = UserStorage(_userStorageAddr);
+
+    uint _userId = _userStorage.addresses(msg.sender);
+
+    require(_userId != 0);
 
     address _tweetStorageAddr = _manager.getAddress("TweetStorage");
     TweetStorage _tweetStorage = TweetStorage(_tweetStorageAddr);
--- test/integration/tweet.js
+++ test/integration/tweet.js
@@ -1,13 +1,24 @@
 const TweetStorage = artifacts.require("TweetStorage");
 const TweetController = artifacts.require("TweetController");
+const UserController = artifacts.require("UserController");
 const utils = require("../utils");
 const { assertVMException } = utils;
 
 contract("tweets", () => {
+	before(async () => {
+		const userCtrl = await UserController.deployed();
+
+		const username = web3.utils.fromAscii("tristan");
+		const firstName = web3.utils.fromAscii("Tristan");
+		const lastName = web3.utils.fromAscii("Edwards");
+
+		await userCtrl.createUser(username, firstName, lastName, "I like building stuff", "example@example.com");
+	});
+
 	it("can create tweet with controller", async () => {
 		const controller = await TweetController.deployed();
 
-		const tx = await controller.createTweet(1, "Hello world!");
+		const tx = await controller.createTweet("Hello world!");
 
 		assert.isOk(tx);
 	});
@@ -33,5 +44,17 @@ contract("tweets", () => {
 		assert.equal(parseInt(id), 1);
 		assert.equal(text, "Hello world!");
 		assert.equal(parseInt(userId), 1);
+	});
+
+	it("can get all tweets IDs from user", async () => {
+		const storage = await TweetStorage.deployed();
+
+		const userId = 1;
+		const ids = await storage.getTweetIdsFromUser.call(userId);
+
+		const expectedTweetId = 1;
+
+		assert.isOk(Array.isArray(ids));
+		assert.equal(ids[0], expectedTweetId);
 	});
 });
